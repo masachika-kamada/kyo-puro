beffafa389e2398be9e78b3def7a980046e2d09e
masachika <masachika.kmd@gmail.com>
2022-04-12 19:31:27 +0900

    自動テストの環境を整えた

--- script/cptest.sh
+++ script/cptest.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+source C:/Users/MK/Anaconda3/etc/profile.d/conda.sh
+conda activate opencv
+
 problem_name=$1
 test_dir=test/${problem_name}
 base_url=${problem_name%_*}
@@ -9,4 +12,6 @@ if [ ! -e ${test_dir} ]; then
     oj dl -d test/${problem_name} https://atcoder.jp/contests/${base_url}/tasks/${problem_name//-/_}
 fi
 
-oj test -c "python3 problems/${problem_name}.py" -d test/${problem_name}
+oj test -c "python src/${problem_name}.py" -d test/${problem_name}
+
+sleep 10

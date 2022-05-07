4dc1dac6a7f4db4532785a655db03bfcfa6737ad
masachika <masachika.kmd@gmail.com>
2022-05-08 01:22:35 +0900

    fix and add comment

--- src/abc249_c.py
+++ src/abc249_c.py
@@ -1,57 +1,43 @@
 def main():
-    import io
-    import sys
+#     import io
+#     import sys
 
-    _INPUT = """\
-4 2
-abi
-aef
-bc
-acg
+#     _INPUT = """\
+# 4 2
+# abi
+# aef
+# bc
+# acg
 
-    """
-    sys.stdin = io.StringIO(_INPUT)
+#     """
+#     sys.stdin = io.StringIO(_INPUT)
 
-    # n, k = map(int, input().split())
-    # s = []
-    # for i in range(n):
-    #     s.append(input())
-    # a = list("".join(s))
-    # a = list(set(a))
-
-    # cnt = 0
-    # for i in range(n + 1):
-    #     comb = itertools.combinations(s, i)
-    #     for c in comb:
-    #         c = "".join(c)
-    #         count = 0
-    #         for ch in a:
-    #             if c.count(ch) == k:
-    #                 count += 1
-    #         if count > cnt:
-    #             cnt = count
-
-    # print(cnt)
-
-    from collections import defaultdict
+    # nが小さいので全探索可能（bit全探索）
+    # itertools.combinations(s, i)でiを変更して…というのは非効率
+    # 普通に選ぶ・選ばないで2^n
 
     N, K = map(int, input().split())
     S = []
     for i in range(N):
         S.append(input())
     ans = 0
-    n2 = 1 << N
-    # print(n2)
+    n2 = 1 << N  # 左シフト i.e. n2 = 2^n
     for t in range(n2):
-        d = defaultdict()
+        d = {}  # 辞書に文字と出現回数をセットにして格納
         for i in range(N):
-            if t & (0x01 << i) != 0x00:
+            # <bit全探索>
+            # t(カウント)を選ぶ・選ばないを判断したい桁数分右シフト
+            # i.e. 末尾の桁を消す
+            # カウントをi回右にシフトして1と論理積を取る
+            # i.e. 最下位の桁が1かどうかチェックする
+            if (t >> i) & 1:
+                # 文字列に含まれるアルファベットを辞書に格納
                 for s in S[i]:
                     if s in d:
                         d[s] += 1
                     else:
                         d[s] = 1
-        cnt = 0
+        cnt = 0  # 出現回数がkだった文字の個数をカウント
         for k, v in d.items():
             if v == K:
                 cnt += 1

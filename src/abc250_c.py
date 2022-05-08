b0b671eafe7b704a165e25d692891468710e9780
masachika <masachika.kmd@gmail.com>
2022-05-09 00:17:23 +0900

    fix

--- src/abc250_c.py
+++ src/abc250_c.py
@@ -3,30 +3,40 @@ def main():
 #     import sys
 
 #     _INPUT = """\
-# 10 6
+# 5 5
 # 1
-# 5
 # 2
-# 9
-# 6
-# 6
+# 3
+# 4
+# 5
 
 #     """
 #     sys.stdin = io.StringIO(_INPUT)
 
     n, q = map(int, input().split())
     a = list(range(1, n + 1))
-    d = dict(zip(a, a))
+    b = list(range(0, n))
+    d = dict(zip(a, b))
     # key: num, val: idx
     for i in range(q):
-        x = int(input())
-        # idx = a.index(x)
-        idx = d[x]
+        num = int(input())
+        """
+        idx = a.index(num)とした部分がO(n)の計算量
+        これが原因で時間内に終了しない
+        indexを辞書に保存することで解決
+        """
+        # idx = a.index(num)
+        idx = d[num]
         if idx == n - 1:
-            # a[idx], a[idx - 1] = a[idx - 1], a[idx]
-            d[x], a[idx - 1] = a[idx - 1], a[idx]
+            # 値の変更
+            a[idx], a[idx - 1] = a[idx - 1], a[idx]
+            # indexの変更
+            d[a[idx]], d[a[idx - 1]] = d[a[idx - 1]], d[a[idx]]
         else:
+            # 値の変更
             a[idx], a[idx + 1] = a[idx + 1], a[idx]
+            # indexの変更
+            d[a[idx]], d[a[idx + 1]] = d[a[idx + 1]], d[a[idx]]
     print(*a)
 
 

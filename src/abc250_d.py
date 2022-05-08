c97a62628ab15ff83b2e5a0f2313c19afda26369
masachika <masachika.kmd@gmail.com>
2022-05-09 03:32:41 +0900

    fix

--- src/abc250_d.py
+++ src/abc250_d.py
@@ -1,44 +1,39 @@
-def prime(n):  # エラトステネスのふるい
-    candidate = list(range(2, n + 1))  # 候補
-    prime = []
-    limit = n**0.5 + 1  # +1がないと平方数を素数にしてしまう
-
-    while True:
-        p = candidate[0]
-        if limit <= p:
-            prime.extend(candidate)  # この時点での残っているのは素数
-            break
-        prime.append(p)  # pを素数リストに登録
-
-        candidate = [i for i in candidate if i % p != 0]
+def get_prime(n):
+    if n < 2:
+        return []
+    candidate = list(range(n + 1))
+    candidate[1] = 0
+    for i in candidate:
+        if i > 1:
+            for j in range(i + i, len(candidate), i):
+                candidate[j] = 0
+    prime = [x for x in candidate if x != 0]
     return prime
 
 
 def main():
-    import io
-    import sys
     import bisect
-
-    _INPUT = """\
-250
-
-    """
-    sys.stdin = io.StringIO(_INPUT)
 
     n = int(input())
     cnt = 0
-    if n <= 1:
+    # qを得るための素数テーブル
+    sosu = get_prime(round((n / 2) ** (1 / 3)))
+    if len(sosu) <= 1:
         print(0)
         exit()
-    u = int((n / 2) ** (1 / 3))
-    sosu = prime(u)
-    ps = prime((int(n ** 0.25)) // 1)
+    # pの候補となる素数テーブル
+    ps = get_prime(round(n ** 0.25))
     for p in ps:
-        qs = int((n / p) ** (1 / 3) // 1)
-        index = bisect.bisect_left(sosu, qs)
-        print(p, qs, index)
-        cnt += index + 1
-    print(sosu)
+        # pから計算される天井を求める
+        # 計算誤差で過小評価されることがある
+        # 一度大きめに取って条件式で補正
+        q = (n / p) ** (1 / 3) + 1
+        idx_q = bisect.bisect_right(sosu, q) - 1
+        idx_p = bisect.bisect_right(sosu, p) - 1
+        while sosu[idx_p] * (sosu[idx_q] ** 3) > n:
+            idx_q -= 1
+        if sosu[idx_p] < sosu[idx_q]:
+            cnt += idx_q - idx_p
     print(cnt)
 
 

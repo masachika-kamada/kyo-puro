091be374a62e876dc11e659363ea6e2a8cc60529
masachika <masachika.kmd@gmail.com>
2022-05-12 09:05:19 +0900

    add arc139_a

---
+++ src/arc139_a.py
@@ -0,0 +1,24 @@
+def main():
+#     import io
+#     import sys
+
+#     _INPUT = """\
+# 8
+# 2 0 2 2 0 4 2 4
+
+#     """
+#     sys.stdin = io.StringIO(_INPUT)
+
+    N = int(input())
+    T = list(map(int, input().split()))
+
+    ans = 0
+    for i in range(N):
+        plus = 2 ** T[i] - ans % (2 ** T[i])
+        ans += plus
+        if ans % (2 ** (T[i] + 1)) == 0:
+            ans += 2 ** T[i]
+    print(ans)
+
+
+main()

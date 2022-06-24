f065bec04b4fbf0c3c1873da7a70dd0a773c25ff
masachika <masachika.kmd@gmail.com>
2022-06-24 18:44:35 +0900

    fix

--- src/abc256_c.py
+++ src/abc256_c.py
@@ -1,13 +1,35 @@
 def main():
-    import io
-    import sys
+    # import io
+    # import sys
 
-    _INPUT = """\
+    # _INPUT = """\
 
-    """
-    sys.stdin = io.StringIO(_INPUT)
+    # """
+    # sys.stdin = io.StringIO(_INPUT)
 
-    h1, h2, h3, w1, w2, w3 = map(int, input().split())
+    A = list(map(int, input().split()))
+    h = A[:3]
+    w = A[3:]
+
+    ans = 0
+    for i in range(1, 31):
+        for j in range(1, 31):
+            for k in range(1, 31):
+                for l in range(1, 31):
+                    a = h[0] - i - j
+                    b = h[1] - k - l
+                    c = w[0] - i - k
+                    d = w[1] - j - l
+                    e = w[2] - a - b
+                    if min(a, b, c, d, e) > 0 and h[2] == c + d + e:
+                        ans += 1
+    print(ans)
 
 
 main()
+
+# i | j | a
+# --+---+---
+# k | l | b
+# --+---+---
+# c | d | e
